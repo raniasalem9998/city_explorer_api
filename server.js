00dff3c1cbdf3ab391092e1bf4afea853dc6584c
raniasalem9998 <rania.salem9998@gmail.com>
2020-07-29 08:11:19 +0300

    back to 7

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const cors = require('cors');
 const { request } = require('express');
 require('dotenv').config();
 const superagent = require('superagent');
-
 const app = server();
+const pg = require('pg');
 app.use(cors());
-
+const client = new pg.Client(process.env.DATABASE_URL);
 
 const PORT = process.env.PORT || 3000;
 
@@ -54,19 +54,16 @@ function Location(city, data) {
 //   ------------------------------------------------- //
 
 app.get('/weather', (request, response) => {
-  // const dataCall = require('./data/weather.json');
   let cityName = request.query.search_query;
   let APIKEY2 = process.env.WEATHER_API_KEY;
-  // let lat = loca[0];
-  // let lon = loca[1];
   let url2 = `https://api.weatherbit.io/v2.0/forecast/daily?city=${cityName}&key=${APIKEY2}`;
 
   return superagent.get(url2).then((data) => {
     Weather.all = [];
     data.body.data.map(element => {
-        const date = new Date(element.valid_date);
-        let time = date.toString();
-       return new Weather(cityName, element, time.substr(0,15));
+      const date = new Date(element.valid_date);
+      let time = date.toString();
+      return new Weather(cityName, element, time.substr(0, 15));
     });
     response.send(Weather.all);
   });
@@ -80,10 +77,44 @@ function Weather(city, data, date) {
   Weather.all.push(this);
 }
 
+// ------------------------------------------------- //
 
-app.listen(PORT, () => {
+// app.get('/trail', (request, response) => {
+//   let APIKEY3 = process.env.TRAIL_API_KEY;
+//   let lat = loca[0];
+//   let lon = loca[1];
+//   let url3 = `https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lon}&key=${APIKEY3}`;
+
+//   return superagent.get(url3).then((data) => {
+//     Trail.all = [];
+//     data.body.data.map(element => {
+//       return new Trail(element);
+//     });
+//     response.send(Trail.all);
+//   });
+
+// });
+
+
+
+// function Trail(data) {
+//   this.name = data.name;
+//   this.location = data.location;
+//   this.stars = data.stars;
+//   this.star_votes = data.star_votes;
+//   this.trail_url = data.trail_url;
+//   this.conditions = data.conditions;
+//   this.condition_date = trail.conditionDate.split(" ")[0];
+//   this.condition_time = trail.conditionDate.split(" ")[1];
+//   Trial.all.push(this);
+// }
+// ------------------------------------------------- //
+// client.connect.then(() =>{
+  app.listen(PORT, () => {
   console.log('Server is listening to port ', PORT);
 });
+// });
+
 
 //will give each weather with all its properties
 
@@ -91,9 +122,3 @@ app.all('*', (req, res) => {
   res.status(404).send('page not found');
   res.status(500).send('Internal server error');
 });
-
-// ------------------------------------------------- //
-// Note that Heroku will need to have properly named 
-// environment variables setup and populated with your API
-//  keys so that it can fetch live data.
-
